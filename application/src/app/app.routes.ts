ab03a6b4e778bead1a40ee7e9a659630521e139f
emiliano.fau <emiliano.fau@scanntech.com>
2025-08-26 19:55:52 -0300

    avances

--- application/src/app/app.routes.ts
+++ application/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'main-chat',
+    loadChildren: () =>
+      import('./features/main-chat/main-chat.routes').then(m => m.routes)
+  },
+
+  { path: '', redirectTo: 'main-chat', pathMatch: 'full' },
+  { path: '**', redirectTo: 'main-chat' }
+];
---
+++ application/src/app/features/main-chat/main-chat.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import {MainChatComponent} from './main-chat.component';
+
+export const routes: Routes = [
+  { path: '', component: MainChatComponent },
+];
