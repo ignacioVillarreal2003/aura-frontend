0c9d2315af71fe5653259cada59be9e870107248
emiliano.fau <emiliano.fau@scanntech.com>
2025-09-03 20:37:10 -0300

    avances ruteo

--- application/src/app/app.routes.ts
+++ application/src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/auth-page/auth-page').then(m => m.AuthPage)
+  },
+
   {
     path: 'main-chat',
     loadChildren: () =>
-      import('./features/main-chat/main-chat.routes').then(m => m.routes)
+      import('./features/main-chat/main-chat.routes').then(m => m.routes),
+    // canActivate: [authGuard]
   },
 
-  { path: '', redirectTo: 'main-chat', pathMatch: 'full' },
-  { path: '**', redirectTo: 'main-chat' }
+  { path: '**', redirectTo: 'login' }
 ];
--- application/src/app/features/auth/login-form/login-form.ts
+++ application/src/app/features/auth/login-form/login-form.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, inject, ViewChild} from '@angular/core';
 import {InputText} from '../../../shared/components/inputs/input-text/input-text';
 import {BtnText} from '../../../shared/components/buttons/btn-text/btn-text';
+import {Router} from '@angular/router';
+import {AuthService} from '../../../core/services/auth.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   imports: [
     InputText,
-    BtnText
+    BtnText,
+    FormsModule
   ],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css'
 })
 export class LoginForm {
+  private router = inject(Router);
+  private auth = inject(AuthService);
 
+  @ViewChild('emailField', { read: ElementRef }) emailField!: ElementRef<HTMLElement>;
+  @ViewChild('passField',  { read: ElementRef }) passField!:  ElementRef<HTMLElement>;
+
+  email = '';
+  password = '';
+
+  private readValue(host: ElementRef<HTMLElement>): string {
+      const input = host.nativeElement.querySelector('input') as HTMLInputElement | null;
+      return input?.value ?? '';
+  }
+
+  onSubmit(evt: Event) {
+      evt.preventDefault();
+      const email = this.readValue(this.emailField);
+      const pass  = this.readValue(this.passField);
+      this.auth.loginHardcoded(email, pass);
+      this.router.navigateByUrl('/main-chat');
+  }
 }
