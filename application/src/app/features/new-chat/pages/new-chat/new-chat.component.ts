ad628f905cc661330ce7fc6fde1fea8f8d43bb51
emiliano.fau <emiliano.fau@scanntech.com>
2025-10-08 23:32:38 -0300

    terminado

---
+++ application/src/app/features/new-chat/pages/new-chat/new-chat.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-chat',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-chat.component.html',
+  styleUrls: ['./new-chat.component.css']
+})
+export class NewChatComponent {
+  
+}
